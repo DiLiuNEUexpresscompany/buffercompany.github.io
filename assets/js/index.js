1ff3595adf0c49612eda0c7e860505f6cc258834
Johnie Xu <281910378@qq.com>
2017-06-17 21:45:59 +0800

    修改首页2

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	var $worksList = $('.works-list li');
 	var listNum = $worksList.length;
 	for(var i = 0; i < listNum; i++){
-		var $a = $worksList.eq(i).find('a');
+		var $a = $worksList.eq(i).find('.img-wrapper a');
 		var imgSrc = $a.attr('imgsrc');
 		$a.css({
 			'background': 'url('+imgSrc+')',
